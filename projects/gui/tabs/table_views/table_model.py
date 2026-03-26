1a2b3b08dd5730b6c59cf0417c35c5562a636375
thiennt176 <thanhthien176@gmail.com>
2026-03-26 21:17:28 +0700

    feat(table_model): create reusable DynamicTableModel

---
+++ projects/gui/tabs/table_views/table_model.py
@@ -0,0 +1,80 @@
+from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex
+import logging
+from config.logging_config import setup_logging
+
+setup_logging()
+logger = logging.getLogger("table_model")
+
+class DynamicTableModel(QAbstractTableModel):
+    def __init__(self):
+        super().__init__()
+        self._rows = []
+        self._columns = []
+        self._headers = {}
+        
+    def rowCount(self, parent = QModelIndex()):
+        if parent.isValid():
+            return 0
+        return len(self._rows)
+    
+    def columnCount(self, parent = QModelIndex()):
+        if parent.isValid():
+            return 0
+        return len(self._columns)
+    
+    def data(self, index: QModelIndex, role=Qt.DisplayRole):
+        if not index.isValid():
+            return None
+        
+        row = self._rows[index.row()]
+        key = self._columns[index.column()]
+        value = row.get(key)
+        
+        if role == Qt.DisplayRole:
+            if value is None:
+                return ""
+            return str(value)
+            
+        if role == Qt.TextAlignmentRole:
+            if isinstance(value, (int, float)):
+                return Qt.AlignRight | Qt.AlignVCenter
+            return Qt.AlignLeft | Qt.AlignVCenter
+        
+        return None
+    
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if role != Qt.DisplayRole:
+            return None
+        if orientation == Qt.Horizontal:
+            key = self._columns[section]
+            
+            return self._headers.get(key, key)
+        
+        if orientation == Qt.Vertical:
+            return str(section+1)
+        return None
+    
+    def load(self, rows: list[dict], columns: list[str], headers: dict[str,str]):
+        """_summary_
+
+        Args:
+            rows (list[dict]): The query result -> list[dict]
+            columns (list[str]): The keys you want to display, in the order you want
+            headers (dict[str,str]): Custom display name e.g., {"score": "Average Score"}
+        """
+        try:
+            self.beginResetModel()
+            if not rows:
+                raise ValueError("Invalid Data")
+                
+            self._rows = rows
+            self._columns = list(columns)
+            self._headers = headers or {}
+            logger.info("Loaded data into the table view success!")
+            
+            self.endResetModel()
+            return True, "Success"
+        except Exception as e:
+            logger.exception("Error loading data into table view")
+            return False, str(e)
+        
