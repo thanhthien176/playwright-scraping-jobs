c8ea816c601d6737e4a95c2dd1bbf26f248bf741
thiennt176 <thanhthien176@gmail.com>
2026-03-26 21:52:25 +0700

    feat(main): get tables from database to build tree navigation in query_tab

--- projects/main.py
+++ projects/main.py
@@ -2,7 +2,7 @@ import sys
 from PySide6.QtWidgets import QApplication
 from gui.main_window import MainWindow
 from controllers.scraper_controller import ScrapeController
-from projects.threads.write_db_thread import DataWriter
+from threads.write_db_thread import DataWriter
 from config.settings import DB_PATH
 from config.logging_config import setup_logging
 import logging
@@ -13,15 +13,27 @@ logger = logging.getLogger("main")
     
 def main():
     app = QApplication(sys.argv)
-    
-    window = MainWindow()
-    
+        
     writer = DataWriter(DB_PATH)
     writer.start()
+    db = writer.db
+    
+    window = MainWindow(writer)
+    
+    
+    tables = db.tables()
+    table_schema = {}
+    for table in tables:
+        table_name = table.get("name")
+        if table_name:
+            table_schema[table_name] = db.schema(table_name).keys()
+    
+    window.update_tree(table_schema)
     
     scraper_controller = ScrapeController(window=window, writer = writer)
     
     window.show()
+    
     
     exit_code = app.exec()
     
